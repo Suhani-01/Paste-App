e3de819fa03b76f7ea05222d810280e748118540
Suhani <suhaninegi124@gmail.com>
2025-07-21 14:37:47 +0530

    error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Paste from './components/Paste';
 import ViewPaste from './components/ViewPaste';
